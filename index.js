b19f6f738d93a1fbea47eb3f8d1ddf891193f429
xukailin <xukailin@bytedance.com>
2020-03-22 21:01:09 +0800

    feat: first commit
    Change-Id: I5ba8fe1d082046c51af304672e00f9d263cff438

---
+++ index.js
@@ -0,0 +1,20 @@
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const spy = require('./spy.js');
+
+const app = new Koa();
+app.use(bodyParser());
+
+app.use(async ctx => {
+  const params = ctx.request.body;
+  const goods = params.goodsIds.split(','); 
+
+  console.log('goods:', goods);
+  const res = await spy(params.ip, goods);
+
+  ctx.body = res;
+});
+
+app.listen(3000, () => {
+  console.log('server is starting at port 3000!');
+});
---
+++ spy.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+
+async function spy(ip, goodsIds) {
+  const args = [];
+  if (ip) args.push(`--proxy-server=${ip}`);
+  const browser = await puppeteer.launch({
+    args,
+  });
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 0,
+    height: 0,
+    isMobile: true,
+  });
+
+  const goods = [];
+  for (let i = 0; i < goodsIds.length; i++) {
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
+    const url = `https://m.jiaoyimao.com/goods/${goodsIds[i]}.html`;
+    await page.goto(url, {waitUntil: 'networkidle0'});
+
+    let content = await page.content();
+    const reg =  /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+    while (reg.test(content)) {
+      content = content.replace(reg, '');
+    }
+		goods.push(content.replace(/\<script.*\<\/script\>/g, ''));
+  }
+
+  await browser.close();
+
+  return goods;
+}
+
+module.exports = spy;
